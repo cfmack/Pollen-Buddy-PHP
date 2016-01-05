7751ebb87fa8cd6e4ddd75485d70ff3f22247565
cmack <cfm4023>
2016-01-04 22:29:44 -0600

    Added caching feature Started examining getting the "key" and "level" mapping

--- PollenBuddy.php
+++ PollenBuddy.php
@@ -12,6 +12,14 @@ class PollenBuddy {
     private $dates = array();
     private $levels = array();
     private $fourDayForecast;
+    
+    // added caching layer to prevent pooling same URL in a day
+    private $cacheLifeHour = false;
+    private $cacheTempFolder = './tmp';
+  
+    // file prefix for cache names
+    const CACHE_PREFIX = 'pollen-cache-';
+    const COLOR_STYLE = 'background-color';
 
     // Wunderground's Pollen API with zipcode GET parameter
     const WUNDERGROUND_URL = "http://www.wunderground.com/DisplayPollen.asp?Zipcode=";
@@ -27,9 +35,11 @@ class PollenBuddy {
      * @param  Integer $zipcode An US-based zipcode
      * @return mixed   $data    Content of the site
      */
-    public function PollenBuddy($zipcode) {
+    public function PollenBuddy($zipcode, $cacheLifeHour = false) {
         $this->zipcode = $zipcode;
-        $this->html = file_get_html(PollenBuddy::WUNDERGROUND_URL . $zipcode);
+        
+        $uri = PollenBuddy::WUNDERGROUND_URL . $zipcode;
+        $this->setHtml($uri, $cacheLifeHour);
     }
 
     /**
@@ -116,12 +126,53 @@ class PollenBuddy {
         return $this->fourDayForecast;
     }
 
+    public function getKeys() {
+    
+    	$keyTexts = array();
+    	$keyColors = array();
+    	
+    	// Iterate through the four dates [Wunderground only has four day
+    	// pollen prediction]
+    	for($i = 0; $i < 4; $i++) {
+    
+    		// Get the raw date
+    		$rawText = $this->html
+    		->find("td.key div", $i)
+    		->plaintext;
+    
+    		// Get the raw color
+    		$rawColor =  $this->html
+    		->find("td.key div", $i)
+    		->style;
+    
+    		//
+    		$colors = $this->breakCSS($rawColor);
+    		$color = '';
+    		if (isset($colors[PollenBuddy::COLOR_STYLE])) {
+    			$color = $colors[PollenBuddy::COLOR_STYLE];
+    		}
+    		
+    		// Push each date to the dates array
+    		array_push($keyTexts, $rawText);
+    		// Push each date to the levels array
+    		array_push($keyColors, $color);
+    	}
+    
+    	$keys = array_combine(
+    			$keyTexts,
+    			$keyColors
+    	);
+    
+    	return $keys;
+    }
+    
     /**
      * Get the four forecasted dates
+     * 
      * @return array Four forecasted dates
      */
     public function getFourDates() {
-        return $this->dates;
+    	return $this->dates;
     }
 
     /**
@@ -131,4 +182,43 @@ class PollenBuddy {
     public function getFourLevels() {
         return $this->levels;
     }
+    
+    private function setHtml($uri, $cacheLifeHour) {
+    	if ($cacheLifeHour == false) {
+    		$this->html = file_get_html($uri);
+    	}
+    	else {
+    		$nowDate = new DateTime("now");
+    		//$cacheDate->sub(new DateInterval("PT" . abs($cacheLifeHour) . "H")); 
+    		
+    		
+    		$file = $this->cacheTempFolder . '/' . PollenBuddy::CACHE_PREFIX . $this->zipcode . '.tmp';
+    		
+    		if (!file_exists($file) || (time() - filemtime($file) >= ($cacheLifeHour * 3600))) {
+    			file_put_contents($file, file_get_html($uri));
+    		}
+    		    		
+    		$this->html = file_get_html($file);
+    	}
+    }
+    
+    /**
+     * Based on http://stackoverflow.com/a/1215128
+     * 
+     * @param unknown $css
+     * @return Ambigous <multitype:, string>
+     */
+    private function breakCSS($css) {
+	    $results = array();
+	
+	    foreach(explode(';', $css) AS $attr) {
+	    	if (strlen(trim($attr)) > 0) // for missing semicolon on last element, which is legal
+	    	{
+	    		list($name, $value) = explode(':', $attr);
+	    		$results[trim($name)] = trim($value);
+	    	}
+	    }
+	    
+	    return $results;
+	}
 }
--- TestPollenBuddy.php
+++ TestPollenBuddy.php
@@ -6,7 +6,7 @@
 
 require_once("PollenBuddy.php");
 
-$data = new PollenBuddy(19104);
+$data = new PollenBuddy(78758, 10);
 //echo $data->getSiteHTML();
 //echo $data->getCity();
 //echo $data->getZipCode();
@@ -14,3 +14,5 @@ $data = new PollenBuddy(19104);
 //var_dump($data->getFourDates());
 //var_dump($data->getFourLevels());
 var_dump($data->getFourDayForecast());
+
+var_dump($data->getKeys());
